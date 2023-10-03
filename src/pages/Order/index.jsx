98e74e3222de07f8756321705afda4ef5f9de242
arthur <arthur.rios007@gmail.com>
2023-10-03 16:50:52 -0300

    add handle order's amount

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -16,14 +16,80 @@ import { useAuth } from '../../hooks/auth'
 import { useState, useEffect } from 'react'
 import { api } from '../../services/api'
 import defaultDish from '../../assets/dish.svg'
+import { useNavigate } from 'react-router-dom'
 
 export function Order() {
   const { user, order } = useAuth()
 
+  const navigate = useNavigate()
+
   const [dishes, setDishes] = useState([])
   const [items, setItems] = useState([])
   const [orderTotal, setOrderTotal] = useState(0)
-  const [isLoading, setIsLoading] = useState(true)
+  const [orderItems, setOrderItems] = useState(0)
+
+  function handleDecrease(item) {
+    if (item.amount > 0) {
+      const oldItems = JSON.parse(localStorage.getItem('@fexplorer:order'))
+      const existingDishIndex = oldItems.dishes.findIndex(
+        (dish) => dish.dishId === item.id,
+      )
+
+      const updatedOrder = { ...oldItems }
+
+      if (existingDishIndex !== -1) {
+        updatedOrder.dishes[existingDishIndex].amount -= 1
+
+        localStorage.setItem('@fexplorer:order', JSON.stringify(updatedOrder))
+
+        setOrderItems(orderItems - 1)
+        setItems(updatedOrder)
+        setDishes(updatedOrder)
+      }
+    }
+  }
+
+  function handleIncrease(item) {
+    const oldItems = JSON.parse(localStorage.getItem('@fexplorer:order'))
+    const existingDishIndex = oldItems.dishes.findIndex(
+      (dish) => dish.dishId === item.id,
+    )
+
+    const updatedOrder = { ...oldItems }
+
+    if (existingDishIndex !== -1) {
+      updatedOrder.dishes[existingDishIndex].amount += 1
+
+      localStorage.setItem('@fexplorer:order', JSON.stringify(updatedOrder))
+
+      setOrderItems(orderItems + 1)
+      setItems(updatedOrder)
+      setDishes(updatedOrder)
+    }
+  }
+
+  function handleDelete(item) {
+    const oldItems = JSON.parse(localStorage.getItem('@fexplorer:order'))
+    const newItems = oldItems.dishes.filter((dish) => dish.dishId !== item.id)
+    const order = {
+      user_id: user.id,
+      dishes: newItems,
+    }
+    localStorage.setItem('@fexplorer:order', JSON.stringify(order))
+    viewTotalOrder(dishes)
+  }
+
+  function handleOrder() {
+    localStorage.setItem(
+      '@fexplorer:order',
+      JSON.stringify({
+        user_id: user.id,
+        dishes: [],
+      }),
+    )
+    alert('Your order has been received! Thank you!')
+    navigate('/')
+  }
 
   function viewTotalOrder(orderDishes) {
     let total = 0
@@ -66,18 +132,17 @@ export function Order() {
         })
         setDishes(response.data)
         viewTotalOrder(response.data)
-        setIsLoading(false)
       } catch (error) {
         console.error('There was an error fetching dishes:', error)
       }
     }
 
     fetchDishes()
-  }, [])
+  }, [dishes])
 
   return (
     <Container>
-      <Header totalOrder={orderTotal} />
+      <Header totalOrder={orderTotal} orderItems={orderItems} />
       <Main>
         <ReturnButton />
         <h1>My Order</h1>
@@ -100,9 +165,11 @@ export function Order() {
                         <span>$ {item.price.toFixed(2).replace('.', ',')}</span>
                       </NameAndValue>
                       <DishControls>
-                        <TfiMinus />
-                        <Button>Delete</Button>
-                        <TfiPlus />
+                        <TfiMinus onClick={() => handleDecrease(item)} />
+                        <Button onClick={() => handleDelete(item)}>
+                          Delete
+                        </Button>
+                        <TfiPlus onClick={() => handleIncrease(item)} />
                       </DishControls>
                     </div>
                   </Dish>
@@ -113,7 +180,7 @@ export function Order() {
           {items.length > 0 && (
             <Price>
               Total: $ {orderTotal.toFixed(2).replace('.', ',')}
-              <Button disabled>Next</Button>
+              <Button onClick={() => handleOrder()}>Order</Button>
             </Price>
           )}
         </div>
